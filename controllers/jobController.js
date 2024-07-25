3c5ae63eda40ffc7bd5c058cc46bd5feaa68e58d
Sam Maton <sam.maton91@gmail.com>
2024-07-25 15:13:00 +0100

    added status codes

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,8 +1,9 @@
 import Job from '../models/JobModel.js'
+import { StatusCodes } from 'http-status-codes'
 
 export async function getAllJobs(c) {
   const allJobs = await Job.find()
-  c.status(200)
+  c.status(StatusCodes.OK)
   return c.json(allJobs)
 }
 
@@ -12,7 +13,7 @@ export async function getJob(c) {
   const job = await Job.findById(id)
 
   if (!job) {
-    c.status(404)
+    c.status(StatusCodes.NOT_FOUND)
     return c.json({ message: 'Job not found' })
   }
 
@@ -22,7 +23,7 @@ export async function getJob(c) {
 export async function createJob(c) {
   const { company, position } = await c.req.json()
   const job = await Job.create({ company, position })
-  c.status(201)
+  c.status(StatusCodes.CREATED)
   return c.json({ message: 'Job created successfully', data: job })
 }
 
@@ -30,7 +31,7 @@ export async function editJob(c) {
   const { ['content-length']: contentLength, ['content-type']: contentType } =
     c.req.header()
   if (contentLength === '0' || !contentType === 'application/json') {
-    c.status(400)
+    c.status(StatusCodes.BAD_REQUEST)
     return c.json({ message: 'Please send some data to update' })
   }
 
@@ -40,7 +41,7 @@ export async function editJob(c) {
     new: true,
   })
 
-  c.status(200)
+  c.status(StatusCodes.OK)
   return c.json({ message: 'Job updated successfully', data: updatedJob })
 }
 
@@ -48,10 +49,10 @@ export async function deleteJob(c) {
   const id = c.req.param('id')
   const deletedJob = await Job.findByIdAndDelete(id)
   if (!deletedJob) {
-    c.status(404)
+    c.status(StatusCodes.NOT_FOUND)
     return c.json({ message: 'Job not found' })
   }
 
-  c.status(200)
+  c.status(StatusCodes.OK)
   return c.json({ message: 'Job deleted successfully' })
 }
